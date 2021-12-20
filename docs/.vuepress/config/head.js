13b79e08f90e7e1f2968f3db8d2f110fe05348b6
WGwg <956910931@qq.com>
2021-12-20 11:49:05 +0800

    commit

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -11,7 +11,7 @@ module.exports = [
     },
   ],
   ['meta', {name: 'referrer', content: 'no-referrer-when-downgrade'}], // 不蒜子统计
-  ['meta', {name: 'baidu-site-verification', content: 'code-R48tPq7QLf'}],// 百度统计的站长验证
+  ['meta', {name: 'baidu-site-verification', content: 'code-l0A2rfDy4a'}],// 百度统计的站长验证
   ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
   // [
   //   'script',
