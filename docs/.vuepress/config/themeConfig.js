67082f454c184f1e353f974960e6817f455d28f9
isgangzi <956910931@qq.com>
2022-01-14 19:51:49 +0800

    feat：添加文章摘要，修改语雀链接，删除github导航栏

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -7,7 +7,7 @@ module.exports = {
   smoothScroll: true,
   sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
   // logo: '/img/logo.png', // 导航栏logo
-  repo: 'https://github.com/isgangzi/weig-Blog', // 导航栏右侧生成Github链接
+  // repo: 'https://github.com/isgangzi/weig-Blog', // 导航栏右侧生成Github链接
   searchMaxSuggestions: 10, // 搜索结果显示最大数
   lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
   docsDir: 'docs', // 编辑的文件夹
@@ -74,7 +74,7 @@ module.exports = {
       {
         iconClass: 'icon-yuque',
         title: '语雀',
-        link: 'https://www.yuque.com/wgg',
+        link: 'https://www.yuque.com/weig',
       },
       {
         iconClass: 'icon-github',
